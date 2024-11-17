b66a31ac66ab95185c5cfe1b7d43124b5876e2a9
mohdjishin <whoistcp@protonmail.com>
2024-11-17 12:53:03 +0530

    made admin authenticated. interceptor  updated

--- internal/middlewares/admin.go
+++ internal/middlewares/admin.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/mohdjishin/order-inventory-management/internal/models"
+	log "github.com/mohdjishin/order-inventory-management/logger"
+)
+
+// OnlyAdmin middleware checks if the user is an admin
+func OnlyAdmin(c fiber.Ctx) error {
+	role, ok := c.Locals(CtxRoleKey{}).(string)
+	if !ok {
+		log.Error("Failed to extract role from context")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Failed to extract role from context",
+		})
+	}
+
+	if role != models.SuperAdmin.String() {
+		log.Error("User is not an admin")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "User is not an admin",
+		})
+	}
+
+	return c.Next()
+}
--- internal/routes/admin.go
+++ internal/routes/admin.go
@@ -3,11 +3,15 @@ package routes
 import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/mohdjishin/order-inventory-management/internal/handlers"
+	middleware "github.com/mohdjishin/order-inventory-management/internal/middlewares"
 )
 
 func RegisterAdminRoutes(app fiber.Router) {
 	adminGroup := app.Group("/")
 	// TODO MIDDLEWARES tobe implemented.
+	adminGroup.Use(middleware.AuthMiddleware)
+	adminGroup.Use(middleware.OnlyAdmin)
+
 	adminGroup.Post("/approve-supplier/", handlers.ApproveSupplier)
 
 	adminGroup.Get("/approved-suppliers", handlers.ListApprovedSuppliers)
